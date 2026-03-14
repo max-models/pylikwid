084a2e0637204b58e68c123c4cc782a47c5f2668
Max <max.lindqvist@ipp.mpg.de>
2026-03-14 17:54:14 +0100

    Removed if (topo_initialized == 1) guard in likwid_finalizetopology

--- src/pylikwid/pylikwid.c
+++ src/pylikwid/pylikwid.c
@@ -389,13 +389,10 @@ likwid_inittopology(PyObject *self, PyObject *args)
 static PyObject *
 likwid_finalizetopology(PyObject *self, PyObject *args)
 {
-    if (topo_initialized == 1)
-    {
-        topology_finalize();
-        topo_initialized = 0;
-        cputopo = NULL;
-        cpuinfo = NULL;
-    }
+    topology_finalize();
+    topo_initialized = 0;
+    cputopo = NULL;
+    cpuinfo = NULL;
     Py_RETURN_NONE;
 }
 
