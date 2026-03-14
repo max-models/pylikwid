f5ae5b2f900a53f280d321cb603eb3a8d247e9a7
Max <max.lindqvist@ipp.mpg.de>
2026-03-14 14:03:15 +0100

    Reset setup.py

--- setup.py
+++ setup.py
@@ -1,97 +1,165 @@
-import os
-import re
-import glob
-from setuptools import setup, Extension
+import os, os.path, glob, re, sys
+from distutils.core import setup, Extension
+
+
+# Utility function to read the README.md file.
+# Used for the long_description.  It"s nice, because now 1) we have a top level
+# README.md file and 2) it"s easier to type in the README.md file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+ver_regex = re.compile(r"so.(\d+)[.]*(\d*)")
 
 
 def generic_iglob(pattern):
-	return glob.iglob(pattern, recursive=True)
+    result = None
+    if sys.version_info.major == 3 and sys.version_info.minor > 4:
+        result = glob.iglob(pattern, recursive=True)
+    else:
+        result = glob.iglob(pattern)
+    return result
 
 
 def find_file(path_iterator):
-	if not path_iterator:
-		return None
-	for path in path_iterator:
-		if os.path.exists(path):
-			return path
-	return None
+    path = None
+    path_to_explore = None
+    is_searching = True
+    if path_iterator:
+        while is_searching:
+            try:
+                path_to_explore = next(path_iterator)
+                if os.path.exists(path_to_explore):
+                    path = path_to_explore
+                is_searching = False
+            except StopIteration:
+                is_searching = False
+    return path
 
 
 def get_hierarchy():
-	include_path = None
-	library_path = None
-	library = None
-	prefix_path = os.getenv('LIKWID_PREFIX', None)
-	library_pattern = 'lib*/liblikwid.so*'
+    print("Searching for LIKWID installation in all folders in $PATH, $LIKWID_PREFIX and /usr/local")
+    print("If you can use LIKWID on the CLI but it is not found, use LIKWID_PREFIX=$(realpath $(dirname $(which likwid-topology))/..)")
+    include_path = None
+    library_path = None
+    library = None
+    prefix_path = os.getenv('LIKWID_PREFIX', None)
+    library_pattern = 'lib*/liblikwid.so*'
+    if prefix_path is not None:
+        iterator = generic_iglob(os.path.join(prefix_path, library_pattern))
+        library = find_file(iterator)
+        if library is not None:
+            library_path = os.path.dirname(library)
+    if library is None:
+        paths_iterator = iter(os.environ["PATH"].split(":"))
+        is_searching = True
+        while is_searching:
+            try:
+                path = next(paths_iterator)
+                prefix_path = os.path.abspath(os.path.join(path, '..'))
+                iterator = generic_iglob(os.path.join(prefix_path, library_pattern))
+                library = find_file(iterator)
+                if library is not None:
+                    library_path = os.path.dirname(library)
+                    is_searching = False
+            except StopIteration:
+                is_searching = False
+        if library is None:
+            prefix_path = '/usr/local'
+            iterator = generic_iglob(os.path.join(prefix_path, library_pattern))
+            library = find_file(iterator)
+            if library is not None:
+                library_path = os.path.dirname(library)
 
-	if prefix_path is not None:
-		library = find_file(generic_iglob(os.path.join(prefix_path, library_pattern)))
-		if library is not None:
-			library_path = os.path.dirname(library)
+    include_path = os.path.join(prefix_path, 'include')
+    if not os.path.exists(os.path.join(include_path, 'likwid.h')):
+        iterator = generic_iglob(prefix_path + '**/likwid.h')
+        include_path = find_file(iterator)
+    if prefix_path is None or not os.path.exists(prefix_path):
+        raise Exception('Error the likwid prefix directory was not found')
+    if library is None or not os.path.exists(library):
+        raise Exception('Error the likwid library was not found')
+    if library_path is None or not os.path.exists(library_path):
+        raise Exception('Error the likwid library directory was not found')
+    if include_path is None or not os.path.exists(include_path):
+        raise Exception('Error the likwid include directory was not found')
+    print("Using LIKWID prefix directory at {!s}".format(prefix_path))
+    print("Using LIKWID include directory at {!s}".format(include_path))
+    print("Using LIKWID library directory at {!s}".format(library_path))
+    print("Using LIKWID library named {!s}".format(library))
+    m = re.match(r"lib(.*)\.so", os.path.basename(library))
+    if m:
+        library = m.group(1)
+    return prefix_path, library_path, library, include_path
 
-	if library is None:
-		for path in os.environ.get("PATH", "").split(":"):
-			prefix_path = os.path.abspath(os.path.join(path, '..'))
-			library = find_file(generic_iglob(os.path.join(prefix_path, library_pattern)))
-			if library is not None:
-				library_path = os.path.dirname(library)
-				break
-
-	if library is None:
-		prefix_path = '/usr/local'
-		library = find_file(generic_iglob(os.path.join(prefix_path, library_pattern)))
-		if library is not None:
-			library_path = os.path.dirname(library)
-
-	include_path = os.path.join(prefix_path, 'include') if prefix_path else None
-	if include_path and not os.path.exists(os.path.join(include_path, 'likwid.h')):
-		include_path = find_file(generic_iglob(os.path.join(prefix_path, '**/likwid.h')))
-
-	if not prefix_path or not os.path.exists(prefix_path):
-		raise Exception('Error: the likwid prefix directory was not found')
-	if not library or not os.path.exists(library):
-		raise Exception('Error: the likwid library was not found')
-	if not library_path or not os.path.exists(library_path):
-		raise Exception('Error: the likwid library directory was not found')
-	if not include_path or not os.path.exists(include_path):
-		raise Exception('Error: the likwid include directory was not found')
-
-	m = re.match(r"lib(.*)\.so", os.path.basename(library))
-	if m:
-		library = m.group(1)
-	return prefix_path, library_path, library, include_path
+try:
+    LIKWID_PREFIX, LIKWID_LIBPATH, LIKWID_LIB, LIKWID_INCPATH = get_hierarchy()
+    print(LIKWID_PREFIX, LIKWID_LIBPATH, LIKWID_LIB, LIKWID_INCPATH)
+except Exception as e:
+    print(e)
+    sys.exit(1)
 
 
-def get_extra_compile_args(include_path):
-	extra_args = []
-	if os.environ.get("LIKWID_NVMON") not in (None, "0"):
-		extra_args.append("-DLIKWID_NVMON")
-	header = os.path.join(include_path, 'likwid.h')
-	with open(header) as f:
-		for line in f:
-			if not line.startswith("#define LIKWID_VERSION"):
-				continue
-			major, release, minor = line.split()[-1].strip('"').split(".")
-			extra_args += [
-				f"-DLIKWID_MAJOR={major}",
-				f"-DLIKWID_RELEASE={release}",
-				f"-DLIKWID_MINOR={minor}",
-			]
-			break
-	return extra_args
+def get_extra_compile_args():
+    extra_args = []
+
+    # Add nvmon definition if set to non-zero value in environment.
+    if os.environ.get("LIKWID_NVMON") not in (None, "0"):
+        extra_args.append("-DLIKWID_NVMON")
+
+    # Add likwid version definitions from likwid.h.
+    likwid_header_path = f"{LIKWID_INCPATH}/likwid.h"
+    with open(likwid_header_path) as f:
+        for line in f:
+            if not line.startswith("#define LIKWID_VERSION"):
+                continue
+            major, release, minor = line.split()[-1].strip('"').split(".")
+            extra_args.extend([
+                f"-DLIKWID_MAJOR={major}",
+                f"-DLIKWID_RELEASE={release}",
+                f"-DLIKWID_MINOR={minor}",
+            ])
+            break
+
+    return extra_args
 
 
-_, lib_path, lib, inc_path = get_hierarchy()
+pylikwid = Extension("pylikwid",
+                     include_dirs=[LIKWID_INCPATH],
+                     libraries=[LIKWID_LIB],
+                     library_dirs=[LIKWID_LIBPATH],
+                     extra_compile_args=get_extra_compile_args(),
+                     sources=["pylikwid.c"])
 
 setup(
-	ext_modules=[
-		Extension(
-			"pylikwid.pylikwid",
-			sources=["src/pylikwid/pylikwid.c"],
-			include_dirs=[inc_path],
-			libraries=[lib],
-			library_dirs=[lib_path],
-			extra_compile_args=get_extra_compile_args(inc_path),
-		)
-	]
+    name="pylikwid",
+    version="0.4.2",
+    author="Thomas Roehl",
+    author_email="thomas.roehl@googlemail.com",
+    description="A Python module to access the function of the LIKWID library",
+    long_description=read("README.rst"),
+    license="GPLv2",
+    keywords="hpc performance benchmark analysis",
+    url="https://github.com/RRZE-HPC/pylikwid",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+    package_data={
+        "pylikwid": ["pylikwid.c", "README.rst", "LICENSE"],
+        "tests": ["tests/*.py"],
+    },
+    ext_modules=[pylikwid],
 )
