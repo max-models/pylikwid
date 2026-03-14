64439c74f224e9330abeb50fd450ef9fc6e8930a
Max <max.lindqvist@ipp.mpg.de>
2026-03-14 15:20:19 +0100

    bugfix: Added get_sources() to add bstrlib

--- setup.py
+++ setup.py
@@ -127,17 +127,31 @@ def get_libraries():
         libs.append("bstr")
     return libs
 
+def get_sources():
+    sources = ["src/pylikwid/pylikwid.c"]
+    for candidate in [
+        os.path.join(LIKWID_INCPATH, "bstrlib.c"),
+        "/usr/local/include/bstrlib.c",
+        "/usr/include/bstrlib.c",
+    ]:
+        if os.path.exists(candidate):
+            print(f"Adding bstrlib from {candidate}")
+            sources.append(candidate)
+            break
+    return sources
+
 pylikwid = Extension("pylikwid.pylikwid",
                      include_dirs=["src/pylikwid", LIKWID_INCPATH],
                      libraries=get_libraries(),
                      library_dirs=[LIKWID_LIBPATH],
                      runtime_library_dirs=[LIKWID_LIBPATH],
                      extra_compile_args=get_extra_compile_args(),
-                     sources=["src/pylikwid/pylikwid.c"],
+                     sources=get_sources(),
+                     #  sources=["src/pylikwid/pylikwid.c"],
                      # Bugfix for static linking
-                    #  extra_link_args=["-Wl,--whole-archive",
-                    #                   f"-l:liblikwid.so",
-                    #                   "-Wl,--no-whole-archive"],
+                     #  extra_link_args=["-Wl,--whole-archive",
+                     #                   f"-l:liblikwid.so",
+                     #                   "-Wl,--no-whole-archive"],
                      py_limited_api=True,
 )
 
